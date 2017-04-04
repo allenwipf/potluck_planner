6880fe093bc2afeea1a690921a3f412ebfba5539
Sumeet Jain <sumeet@sumeetjain.com>
2017-04-04 14:10:03 -0500

    Spacing

--- app/models/potluck.rb
+++ app/models/potluck.rb
@@ -1,11 +1,11 @@
 class Potluck < ActiveRecord::Base
 	has_many :items  #, dependent :destroy
 	belongs_to :user
+
 	validates :title, presence: true
 	validates :location, presence: true
 	validates :description, presence: true
 	validates :date, presence: true
 	validates :time, presence: true
 	validates :user_id, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
-
 	has_many :potlucks
+
 	validates :name, presence: true
 	validates :email, presence: true
 	validates :password, presence: true
-
-
 end
