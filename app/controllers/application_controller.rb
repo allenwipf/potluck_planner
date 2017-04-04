06def776242b5bdeb29c036d8a7bd8fb3a4a6285
Sumeet Jain <sumeet@sumeetjain.com>
2017-04-04 15:04:51 -0500

    Move some code into the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  before_filter :load_current_user
+  before_filter :load_new_user
+
+  def load_new_user
+    if @current_user.nil?
+      @user = User.new
+    end
+  end
+
+  def load_current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    if session[:user_id]
-      @current_user = User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
   end
 
   def potluck_view
