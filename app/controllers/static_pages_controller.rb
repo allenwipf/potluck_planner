7768d2d18b2fd9d72e80b4e6ad709d9d34b38d41
Sumeet Jain <sumeet@sumeetjain.com>
2017-04-04 14:35:29 -0500

    Set @current_user conditionally

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
   end
 
   def potluck_view
