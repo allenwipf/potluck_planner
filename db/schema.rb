f6b52c2e67f329e025ae3b2beb9aa57b23c08335
Allen Wipf <allenwipf@msn.com>
2017-04-05 12:34:05 -0500

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405161412) do
+ActiveRecord::Schema.define(version: 20170405172929) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
