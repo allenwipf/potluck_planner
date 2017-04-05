b4951131e0355fd1d7fce8413af5004f6ac8cdfd
Allen Wipf <allenwipf@msn.com>
2017-04-05 12:02:40 -0500

    secure login system

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,24 +1,23 @@
 class LoginController < ApplicationController
 
 
-	def create
-	  user = User.find_by(:email => params[:user][:email], :password => params[:user][:password])
-
-	  if !user.nil?
-	    session[:user_id] = user.id
+  def create
+	 user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+    	session[:user_id] = user.id
 	    flash[:success] = "You are logged In!"
 	    redirect_to '/user/show'
-	  else
-	    flash[:danger] = "Incorrect email or password"
-	    redirect_to root_path
-	  end
-
+    else
+    	flash[:danger] = 'Wrong username or password'
+    	redirect_to '/'
+    end
   end
 
 
 	def destroy
 		session[:user_id] = nil
 		flash[:success] = "Successfully logged out"
-  	redirect_to root_path
+  	    redirect_to root_path
 	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :email, presence: true
-	validates :password, presence: true
+
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
+
 end
