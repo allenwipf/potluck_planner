ed55fc05a5204b8af4a5a49644fd1c240adc2cdb
Allen Wipf <allenwipf@msn.com>
2017-04-04 19:20:12 -0500

    working on login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,21 +1,24 @@
 class LoginController < ApplicationController
 
+
 	def create
-	  user = User.find_by(:email => params[:email], :password => params[:password])
+	  user = User.find_by(:email => params[:user][:email], :password => params[:user][:password])
+
 	  if !user.nil?
 	    session[:user_id] = user.id
 	    flash[:success] = "You are logged In!"
-	    redirect_to 'user/show'
+	    redirect_to '/user/show'
 	  else
 	    flash[:danger] = "Incorrect email or password"
-	    redirect_to '/'
+	    redirect_to root_path
 	  end
+
   end
 
-	def destroy
 
+	def destroy
 		session[:user_id] = nil
 		flash[:success] = "Successfully logged out"
-  		redirect_to root_path
+  	redirect_to root_path
 	end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,29 +1,16 @@
 class UserController < ApplicationController
 
-	def index
-		@current_user = User.find(1)
-	end
-
-  def create
-    @user = User.new(user_params)
- 
-    if !User.exists?(@user.email) && @user.save
-      	session[:user_id] = @user.id
-      	flash[:success] = "Welcome to your Potluck Manager!"
-      	redirect_to @user
-    else
-    	session[:error] = "User already exists."
-      render 'new'
-    end
-  end
-
-	def new
-		@user = User.new
-	end
-
-	def show
-		# @current_user = User.find(@current_user.id)
-	end
+	def create
+    	@user = User.new(user_params)
+	    if !User.exists?(@user.email) && @user.save
+	      	session[:user_id] = @user.id
+	      	flash[:success] = "Welcome to your Potluck Manager!"
+	      	redirect_to @user
+	    else
+	    	session[:error] = "User already exists."
+	      render 'new'
+	    end
+  	end
 
 	def user_params
 		params.require(:user).permit(:name, :email, :password)
